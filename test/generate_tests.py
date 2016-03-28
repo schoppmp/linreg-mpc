73ad5babf05cf29a67602a22246bf117c7446d32
agascon <adriagascon@gmail.com>
2016-03-28 19:17:38 +0100

    Added testcase generation for our instances, namely a masked linear system and corresponding masks

--- test/generate_tests.py
+++ test/generate_tests.py
@@ -4,21 +4,59 @@ import argparse
 import numpy
 import os
 
+
+def write_system(A, b, filepath = None):
+    with open(filepath, 'w') as f:
+        f.write('{0} {1}\n'.format(A.shape[0], A.shape[1]))
+        for i in range(A.shape[0]):
+            for j in range(A.shape[1]):
+                f.write(str(A[i][j]))
+                f.write(' ')
+            f.write('\n')
+        f.write('{0}\n'.format(b.shape[0]))
+        for i in range(b.shape[0]):
+            f.write(str(b[i]))
+
+def write_sls_instance(A, A_, b, b_, filepath = None):
+    with open(filepath, 'w') as f:
+        f.write('{0} {1}\n'.format(A.shape[0], A.shape[1]))
+        for i in range(A.shape[0]):
+            for j in range(A.shape[1]):
+                f.write(str(A[i][j]))
+                f.write(' ')
+            f.write('\n')
+        f.write('{0}\n'.format(b.shape[0]))
+        for i in range(b.shape[0]):
+            f.write(str(b[i]))
+            f.write(' ')
+        f.write('\n')
+        f.write('{0} {1}\n'.format(A.shape[0], A.shape[1]))
+        for i in range(A_.shape[0]):
+            for j in range(A_.shape[1]):
+                f.write(str(A_[i][j]))
+                f.write(' ')
+            f.write('\n')
+        f.write('{0}\n'.format(b_.shape[0]))
+        for i in range(b_.shape[0]):
+            f.write(str(b_[i]))
+            f.write(' ')
+
+def generate_sls_instance(n, d, lambda_, filepath):
+    (A, b) = generate_lin_system(n, d, lambda_)
+    mask_A = random.rand(d, d)
+    mask_b = random.rand(d)
+    masked_A = A + mask_A
+    masked_b = b + mask_b
+    if filepath:
+        write_sls_instance(masked_A, mask_A, masked_b, mask_b, filepath)
+    return (masked_A, mask_A, masked_b, mask_b)
+    
 def generate_lin_system(n, d, lambda_, filepath=None):
     (X, y, beta, e) = generate_lin_regression(n, d)
     A = X.T.dot(X) + numpy.inner(numpy.identity(d), lambda_)
     b = X.T.dot(y)
     if filepath:
-        with open(filepath, 'w') as f:
-            f.write('{0} {1}\n'.format(A.shape[0], A.shape[1]))
-            for i in range(A.shape[0]):
-                for j in range(A.shape[1]):
-                    f.write(str(A[i][j]))
-                    f.write(' ')
-                f.write('\n')
-            f.write('{0}\n'.format(b.shape[0]))
-            for i in range(b.shape[0]):
-                f.write(str(b[i]))
+        write_system(A, b)
     return (A, b)
 
 def generate_lin_regression(n, d, filepath=None):
@@ -43,10 +81,10 @@ if __name__ == "__main__":
     VERBOSE = args.verbose
     
     for i in range(args.num_matrices):
-        filename_lr = 'test_LR_{0}{1}_{2}.test'.format(args.n, args.d, i)
-        filename_ls = 'test_LS_{0}{1}_{2}.test'.format(args.n, args.d, i)
+        filename_lr = 'test_LR_{0}x{1}_{2}.test'.format(args.n, args.d, i)
+        filename_ls = 'test_LS_{0}x{1}_{2}.test'.format(args.n, args.d, i)
         filepath_lr = os.path.join(args.dest_folder, filename_lr)
         filepath_ls = os.path.join(args.dest_folder, filename_ls)
         #generate_lin_regression(args.n, args.d, filepath_lr)
-        (A, b) = generate_lin_system(args.n, args.d, 3, filepath_ls)
+        (masked_A, mask_A, masked_b, mask_b) = generate_sls_instance(args.n, args.d, 3, filepath_ls)
 
