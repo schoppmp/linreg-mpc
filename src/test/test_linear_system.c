747bb52c6395f0e84b05212eb77f0f7a444f5d64
agascon <adriagascon@gmail.com>
2016-03-29 19:07:01 +0100

    fixed error in instance reading from file

--- src/test/test_linear_system.c
+++ src/test/test_linear_system.c
@@ -4,9 +4,7 @@
 #include "util.h"
 #include "error.h"
 
-
 const int precision = 16;
-#define DIM 4
 
 void read_ls_from_file(int party, char* filepath, linear_system_t* ls){
   FILE *file;
@@ -70,7 +68,7 @@ void read_ls_from_file(int party, char* filepath, linear_system_t* ls){
     res = fscanf(file, "%lf", &b_mask[i]);
     b_mask_fixed_prec[i] = double_to_fixed(b_mask[i], precision);
     b_masked_fixed_prec[i] = b_mask_fixed_prec[i] + b_fixed_prec[i];
-    printf("%f ", b_mask_fixed_prec[i]);
+    printf("%f ", b_mask[i]);
   }
   printf("\n");
 
@@ -93,11 +91,6 @@ void read_ls_from_file(int party, char* filepath, linear_system_t* ls){
   }
 }
 
-/* double a_d[DIM][DIM] = {{11,0.7,-3,-4},{0.7,8,2,-5},{-3,2,5,-6},{-4,-5,-6,123.456}}; */
-/* double mask_a_d[DIM][DIM] = {{10, -0.20, -30, 0},{0.40, 50, -0.60, 0}, {7,8,9,0}, {12,34,56,789}}; */
-/* double b_d[DIM] = {123,-456,7,-0.8}; */
-/* double mask_b_d[DIM] = {555, -666, 0.777, -0.888}; */
-
 int main(int argc, char **argv) {
 	check(argc >= 3, "Usage: %s [Port] [Party] [Input file]", argv[0]);
 	int party = 0;
@@ -109,46 +102,6 @@ int main(int argc, char **argv) {
 	check(party > 0, "Party must be either 1 or 2.");
 	linear_system_t ls;
 	read_ls_from_file(party, argv[3], &ls);
-	for(size_t i = 0; i < ls.b.len; i++) {
-	  printf("\n%lf", ls.b.value[i]);
-	}
-
-	/* int d = DIM; */
-
-	/* // convert test data to fixed point */
-	/* fixed32_t *a = alloca(d*d*sizeof(fixed32_t)); */
-	/* fixed32_t *mask_a = alloca(d*d*sizeof(fixed32_t)); */
-	/* fixed32_t *b = alloca(d*sizeof(fixed32_t)); */
-	/* fixed32_t *mask_b = alloca(d*sizeof(fixed32_t)); */
-
-	/* for(size_t i = 0; i < d; i++) { */
-	/* 	for(size_t j = 0; j < d; j++) { */
-	/* 		mask_a[i*d+j] = double_to_fixed(mask_a_d[i][j], precision); */
-	/* 		a[i*d+j] = double_to_fixed(a_d[i][j], precision) + mask_a[i*d+j]; */
-	/* 	} */
-	/* 	mask_b[i] = double_to_fixed(mask_b_d[i], precision); */
-	/* 	b[i] = double_to_fixed(b_d[i], precision) + mask_b[i]; */
-	/* } */
-
-	/* ls.a.d[0] = ls.a.d[1] = ls.b.len = d; */
-	/* ls.precision = precision; */
-
-	/* // read party */
-	/* int party = 0; */
-	/* if(!strcmp(argv[2], "1")) { */
-	/* 	party = 1; */
-	/* 	ls.a.value = a; */
-	/* 	ls.b.value = b; */
-	/* 	ls.beta.value = NULL; */
-	/* 	ls.beta.len = -1; */
-	/* } else if(!strcmp(argv[2], "2")) { */
-	/* 	party = 2; */
-	/* 	ls.a.value = mask_a; */
-	/* 	ls.b.value = mask_b; */
-	/* 	ls.beta.value = alloca(d*sizeof(fixed32_t)); */
-	/* 	ls.beta.len = d; */
-	/* } */
-	/* check(party > 0, "Party must be either 1 or 2."); */
 
 	ProtocolDesc pd;
 	ocTestUtilTcpOrDie(&pd, party==1, argv[1]);
