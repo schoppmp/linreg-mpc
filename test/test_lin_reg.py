3926ad01573aaf385ca8ebd8944646c554d0f873
agascon <adriagascon@gmail.com>
2016-04-06 19:03:04 +0100

    Minor changes

--- test/test_lin_reg.py
+++ test/test_lin_reg.py
@@ -8,6 +8,67 @@ import os
 import subprocess
 import re
 
+
+def parse_output(n, d, alg, solution, s, filepath_ls_out):
+    print s
+    f = open(filepath_ls_out, 'w')
+    cgd_iter_solutions = []
+    cgd_iter_gate_sizes = []
+    lines = s.split('\n')
+    for line in lines:
+        if alg == 'cgd':
+            m = re.match('m\s+=\s+((\s*[\d\.-]+)+)', line)
+            if m:
+                scaling = map(float, m.group(1).split())
+            m = re.match('((\s*[\d\.-]+)+)', line)
+            if m:
+                scaled_solution = map(float, m.group(1).split())
+                rescaled_solution = [
+                    scaled_solution[i]/scaling[i]
+                    for i in range(len(scaling))]
+                cgd_iter_solutions.append(rescaled_solution)
+            m = re.match('\s*Yao\'s\s+gates\s+count:\s+(\d+)$', line)
+            if m:
+                cgd_iter_gate_sizes.append(int(m.group(1)))
+        m = re.match('Algorithm:\s*(\S+)', line)
+        if m:
+            pass#assert alg == m.group(1)
+        m = re.match('Time\s+elapsed:\s*(\S+)', line)
+        if m:
+            time = float(m.group(1))
+        m = re.match('Number\s+of\s+gates:\s*(\S+)', line)
+        if m:
+            gate_count = int(m.group(1))
+        m = re.match('Result:\s*(.+)', line)
+        if m:
+        #print solution
+        #print cgd_iter_solutions
+            result = map(float, m.group(1).split())
+            error = spatial.distance.euclidean(result, solution)
+            f.write('n d algorithm time error gate_count')
+            f.write('\n{0} {1} {2} {3} {4} {5}'.format(n, d,
+                alg, time, error, gate_count))
+            if alg == 'cgd':
+                gate_count_after_iters =\
+                    gate_count - cgd_iter_gate_sizes[-1]
+                cgd_iter_gate_sizes = map(
+                    lambda x: x+gate_count_after_iters,
+                    cgd_iter_gate_sizes)
+                assert len(cgd_iter_gate_sizes) == len(cgd_iter_solutions),\
+                    '{0}\n{1}\n{2}\n{3}'.format(
+                        len(cgd_iter_solutions),
+                        cgd_iter_solutions,
+                        len(cgd_iter_gate_sizes),
+                        cgd_iter_gate_sizes)
+                f.write('\niter_i error_i gate_count_i')
+                for i in range(len(cgd_iter_solutions)):
+                    error_i = spatial.distance.euclidean(
+                        cgd_iter_solutions[i], solution)
+                    f.write('\n{0} {1} {2}'.format(
+                        i+1, error_i, cgd_iter_gate_sizes[i]))
+    f.close()
+
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
         description='Tests our linear regression implementations. '
@@ -29,6 +90,15 @@ if __name__ == "__main__":
     else:
         algorithms = [args.algorithm]
     VERBOSE = args.verbose
+
+
+#    with open('tmp.in') as f:
+ #       s = f.read()
+  #      parse_output(100, 100, 'cgd', range(100), s, 'tmp.out')
+
+    #sys.exit()
+
+
     for i in range(args.num_examples):
         filename_ls = 'test_LS_{0}x{1}_{2}.test'.format(
             args.n, args.d, i)
@@ -49,55 +119,4 @@ if __name__ == "__main__":
             cmd2 = cmd[:2] + ['2'] + cmd[3:]
             subprocess.Popen(cmd1)
             s = subprocess.check_output(cmd2)
-            print s
-            f = open(filepath_ls_out, 'w')
-            cgd_iter_solutions = []
-            cgd_iter_gate_sizes = []
-            lines = s.split('\n')
-            for line in lines:
-                if alg == 'cgd':
-                    m = re.match('m\s+=\s+((\s*[\d\.-]+)+)', line)
-                    if m:
-                        scaling = map(float, m.group(1).split())
-                    m = re.match('((\s*[\d\.-]+)+)', line)
-                    if m:
-                        scaled_solution = map(float, m.group(1).split())
-                        rescaled_solution = [
-                            scaled_solution[i]/scaling[i]
-                            for i in range(len(scaling))]
-                        cgd_iter_solutions.append(rescaled_solution)
-                    m = re.match('\s*Yao\'s\s+gates\s+count:\s+(\d+)$', line)
-                    if m:
-                        cgd_iter_gate_sizes.append(int(m.group(1)))
-                m = re.match('Algorithm:\s*(\S+)', line)
-                if m:
-                    pass#assert alg == m.group(1)
-                m = re.match('Time\s+elapsed:\s*(\S+)', line)
-                if m:
-                    time = float(m.group(1))
-                m = re.match('Number\s+of\s+gates:\s*(\S+)', line)
-                if m:
-                    gate_count = int(m.group(1))
-                m = re.match('Result:\s*(.+)', line)
-                if m:
-                #print solution
-                #print cgd_iter_solutions
-                    result = map(float, m.group(1).split())
-                    error = spatial.distance.euclidean(result, solution)
-                    f.write('n d algorithm time error gate_count')
-                    f.write('\n{0} {1} {2} {3} {4} {5}'.format(args.n, args.d,
-                        alg, time, error, gate_count))
-                    if alg == 'cgd':
-                        gate_count_after_iters =\
-                            gate_count - cgd_iter_gate_sizes[-1]
-                        cgd_iter_gate_sizes = map(
-                            lambda x: x+gate_count_after_iters,
-                            cgd_iter_gate_sizes)
-                        assert len(cgd_iter_gate_sizes) == len(cgd_iter_solutions)
-                        f.write('\niter_i error_i gate_count_i')
-                        for i in range(len(cgd_iter_solutions)):
-                            error_i = spatial.distance.euclidean(
-                                cgd_iter_solutions[i], solution)
-                            f.write('\n{0} {1} {2}'.format(
-                                i+1, error_i, cgd_iter_gate_sizes[i]))
-            f.close()
+            parse_output(args.n, args.d, alg, solution, s, filepath_ls_out)
