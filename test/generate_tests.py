9fab33a851cd4451ae23734a787c10ee6e51f244
agascon <adriagascon@gmail.com>
2016-06-25 17:24:06 +0100

    Update aws phase 1 experiments script

--- test/generate_tests.py
+++ test/generate_tests.py
@@ -26,7 +26,7 @@ def write_system(A, b, solution, filepath):
             f.write(' ')
 
 
-def write_lr_instance(X, y, solution, filepath, num_parties=1):
+def write_lr_instance(X, y, solution, filepath, num_parties=1, endpoints=None):
     """
     Generates an instance of a vertucally partitioned Linear Regression
     problem ans writes it in [filepath]
@@ -36,34 +36,62 @@ def write_lr_instance(X, y, solution, filepath, num_parties=1):
     assert y.shape[0] == n, y.shape[0]
     assert solution.shape[0] == d
     assert num_parties > 0
+    if endpoints:
+        assert len(endpoints) >= num_parties
     with open(filepath, 'w') as f:
-        f.write('// n d num_parties\n')
+        #f.write('// n d num_parties\n')
         f.write('{0} {1} {2}\n'.format(n, d, num_parties))
+        #f.write('// TI's endpoint\n')
+        if not endpoints:
+            f.write('localhost:{0}\n'.format(1234))
+        else:
+            f.write('{0}\n'.format(endpoints[0]))
+            f.write('{0}\n'.format(endpoints[1]))
         if num_parties > 1:
             # We assume an even partition of (A,b) among the parties
             # Last party gets num_features mod num_parties additional features
             i = 0
-            f.write('// initial index of features from each party\n')
-            for _ in range(num_parties):
-                f.write('{0}\n'.format(i))
+            #f.write('// endpoint and initial index of features from each party\n')
+            for p in range(num_parties):
+                if not endpoints:
+                    f.write('localhost:{0} {1}\n'.format(1234 + p + 2, i))
+                else:
+                    f.write('{0} {1}\n'.format(endpoints[p + 2], i))
                 i += d / num_parties
-        f.write('// X:\n')
+        #f.write('// X:\n')
         f.write('{0} {1}\n'.format(n, d))
         for i in range(n):
             for j in range(d):
                 f.write(str(X[i][j]))
                 f.write(' ')
             f.write('\n')
-        f.write('// y (owned by last party):\n')
+        #f.write('// y (owned by last party):\n')
         f.write('{0}\n'.format(n))
         for i in range(n):
             f.write(str(y[i]))
             f.write(' ')
         f.write('\n')
-        f.write('// beta (solution):\n')
+        #f.write('// beta (solution):\n')
         f.write('{0}\n'.format(d))
         for i in range(d):
             f.write(str(solution[i]))
+            f.write(' ')
+        f.write('\n')
+        #f.write('// XX^T:\n')
+        cov = 1. / (d * n) * X.T.dot(X)
+        b = 1. / (d * n) * X.T.dot(y)
+        assert b.shape[0] == d
+        assert cov.shape[0] == d
+        assert cov.shape[1] == d
+        f.write('{0} {0}\n'.format(d))
+        for i in range(d):
+            for j in range(d):
+                f.write(str(cov[i][j]))
+                f.write(' ')
+            f.write('\n')
+        f.write('{0}\n'.format(d))
+        for i in range(d):
+            f.write(str(b[i]))
             f.write(' ')
 
 
