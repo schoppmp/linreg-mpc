03af8ea33b83e6a385ce567d0f5e36a9f5d5bf87
agascon <adriagascon@gmail.com>
2016-06-26 16:47:22 +0100

    Adapt phase 1 experiments script to run on AWS

--- test/test_phase1_aws.py
+++ test/test_phase1_aws.py
@@ -5,9 +5,9 @@ import paramiko
 import logging
 
 REMOTE_USER = 'ubuntu'
-#KEY_FILE = '/home/ubuntu/.ssh/id_rsa'
-KEY_FILE = '/home/agascon/Desktop/experiments1.pem'
-USE_PUB_IPS = True
+KEY_FILE = '/home/ubuntu/.ssh/id_rsa'
+# Set to True to run this script from a machine outside AWS
+USE_PUB_IPS = False
 
 logger = logging.getLogger(__name__)
 logging.basicConfig(format='%(asctime)s %(message)s')
@@ -35,6 +35,7 @@ if __name__ == "__main__":
         private_endpoints = []
         with open(args.ips_file, 'r') as f:
             for i, line in enumerate(f.readlines()):
+                if not line.strip(): continue
                 public_ips.append(line.split()[0])
                 private_ips.append(line.split()[1])
                 private_endpoints.append(line.split()[1] + ':{0}'.format(
@@ -49,12 +50,19 @@ if __name__ == "__main__":
         client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
         client.connect(hostname=ip, username=REMOTE_USER, pkey=key)
 
-        cmd_cd = 'cd obliv-c; make CFLAGS=\"-DPROFILE_NETWORK\"; cd ..; ' +\
+        logger.info('Compiling in {0}'.format(ip))
+
+
+        cmd = 'cd obliv-c; make CFLAGS=\"-DPROFILE_NETWORK\"; cd ..; ' +\
             'cd secure-distributed-linear-regression; ' +\
             'git stash; git checkout master; git pull; make clean;' +\
+             'git submodule update --init --recursive;' +\
+            'cd lib/absentminded-crypto-kit/;' +\
+            'make OBLIVC_PATH=$(cd ../../../obliv-c && pwd);' +\
+            'cd ../..;' +\
             'make OBLIVC_PATH=$(cd ../obliv-c && pwd) bin/secure_multiplication;' +\
             'killall -9 secure_multiplication'
-        stdin, stdout, stderr = client.exec_command(cmd_cd)
+        stdin, stdout, stderr = client.exec_command(cmd)
         for line in stdout:
             print '... ' + line.strip('\n')
         for line in stderr:
@@ -117,27 +125,30 @@ if __name__ == "__main__":
         client.close()
 
     def retrieve_out_files(party_out_files,
-            remote_dest_folder, local_dest_folder):
-        for f in party_out_files:
-            remote_filepath = os.path.join(remote_dest_folder, f)
-            local_filepath = os.path.join(local_dest_folder, f)
+                        remote_dest_folder, local_dest_folder):
+        for i, f in enumerate(party_out_files):
+            ip = public_ips[i] if USE_PUB_IPS else private_ips[i]
             key = paramiko.RSAKey.from_private_key_file(KEY_FILE)
             client = paramiko.SSHClient()
             client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
             client.connect(hostname=ip, username=REMOTE_USER, pkey=key)
             sftp = client.open_sftp()
-            sftp.get(remote_filepath, local_filepath)
+            sftp.chdir(os.path.join('secure-distributed-linear-regression',
+                remote_dest_folder))
+            logger.info(
+                'Retrieving .exec file {0} from {1} in {2}'.format(
+                    f, remote_dest_folder, ip))
+            sftp.get(f, os.path.join(local_dest_folder, f))
             client.close()
-
 
     for n in [1000, 2000, 5000, 10000, 20000, 50000, 100000, 200000, 500000, 1000000]:
         for d in [10, 20, 50, 100, 200, 500]:
             for p in [2, 5, 10, 20]:  # p is number of data providers (not TI)
                 if p > d:
                     continue
-                logger.info('Running instance n = {0}, d = {1}, p = {2}, run = {3}'.format(
-                    n, d, p, num_examples))
                 for i in range(num_examples):
+                    logger.info('Running instance n = {0}, d = {1}, p = {2}, run = {3}'.format(
+                        n, d, p, i))
                     filename_in = 'test_LR_{0}x{1}_{2}_{3}.in'.format(
                         n, d, p, i)
                     filepath_in = os.path.join(dest_folder, filename_in)
@@ -155,7 +166,7 @@ if __name__ == "__main__":
                             .format(n, d, p, i, party)
                         filepath_exec = os.path.join(
                             dest_folder, filename_exec)
-                        party_exec_files.append(filepath_exec)
+                        party_exec_files.append(filename_exec)
                         cmd = '{0} {1} {2} {3} > {4} {5}'.format(
                             exec_file,
                             filepath_in,
@@ -173,16 +184,20 @@ if __name__ == "__main__":
                             if USE_PUB_IPS else private_ips[party - 1],
                             cmd)
 
-                    retrieve_out_files(party_exec_files, dest_folder)
+                    logger.info('All parties running.')
+
+                    retrieve_out_files(party_exec_files,
+                        dest_folder, dest_folder)
                     data_parties_times = []
-                    for party in range(1, p + 3):
-                        with open(party_exec_files[party], 'r') as f:
-                            filename_out = os.path.splitext(
-                                party_exec_files[party])[0] + '.out'
-                            lines = f.readlines()
-                            print party, lines
-                            if party == 1:  # TI
-                                benchmark_decr = lines[0].split() if len(lines[0].split()) == 3 else lines[1]
-                                ti_time = float(lines[1].split()[1] if len(lines[0].split()) == 3 else lines[0])
-                            else:
-                                data_parties_times.append(float(lines[0].split()[1]))
+                    for filename_exec in party_exec_files:
+                        filepath_exec = os.path.join(
+                            dest_folder, filename_exec)
+                        filename_out = os.path.splitext(
+                            filename_exec)[0] + '.out'
+                        f_exec = open(filepath_exec, 'r')
+                        f_out = open(filepath_exec, 'r')
+                        print filepath_exec
+                        for line in f_exec.readlines():
+                            print line
+                        f_exec.close()
+                        f_out.close()
