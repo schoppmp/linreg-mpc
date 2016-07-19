60f21e251e2393925d9a5719be45fbfe7a783bae
agascon <adriagascon@gmail.com>
2016-07-19 15:59:59 +0000

    Update phase 1 experiments script to parse network data and not recompute existing data

--- test/test_phase1_aws.py
+++ test/test_phase1_aws.py
@@ -3,11 +3,15 @@ import argparse
 import generate_tests as GT
 import paramiko
 import logging
+import re
 
 REMOTE_USER = 'ubuntu'
 KEY_FILE = '/home/ubuntu/.ssh/id_rsa'
 # Set to True to run this script from a machine outside AWS
 USE_PUB_IPS = False
+# Set to True to rerun existing instances
+RECOMPUTE = False
+
 
 logger = logging.getLogger(__name__)
 logging.basicConfig(format='%(asctime)s %(message)s')
@@ -21,13 +25,13 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     precision = 56
-    num_examples = 5
+    num_examples = 3
     exec_file = 'bin/secure_multiplication'
     dest_folder = 'test/experiments/phase1/'
     assert os.path.exists(dest_folder), '{0} does not exist.'.format(
         dest_folder)
-    assert not os.listdir(dest_folder), '{0} is not empty.'.format(
-        dest_folder)
+    #assert not os.listdir(dest_folder), '{0} is not empty.'.format(
+    #    dest_folder)
 
     if args.ips_file:
         public_ips = []
@@ -61,7 +65,7 @@ if __name__ == "__main__":
             'cd lib/absentminded-crypto-kit/;' +\
             'make OBLIVC_PATH=$(cd ../../../obliv-c && pwd);' +\
             'cd ../..;' +\
-            'make OBLIVC_PATH=$(cd ../obliv-c && pwd) bin/secure_multiplication;' +\ 
+            'make OBLIVC_PATH=$(cd ../obliv-c && pwd) bin/secure_multiplication;' +\
             'killall -9 secure_multiplication'
         '''
         cmd = 'killall -9 secure_multiplication; ' +\
@@ -81,7 +85,7 @@ if __name__ == "__main__":
             remote_dest_folder, local_dest_folder,
             local_input_filepath, ip, exec_cmd):
 
-        logger.info('Connecting to {}:'.format(ip))
+        logger.info('Connecting to {}...'.format(ip))
         key = paramiko.RSAKey.from_private_key_file(KEY_FILE)
         client = paramiko.SSHClient()
         client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
@@ -113,6 +117,7 @@ if __name__ == "__main__":
             'secure-distributed-linear-regression', remote_dest_folder)
         mkdir_p(sftp, remote_dest_folder)
         input_filename = os.path.basename(local_input_filepath)
+        logger.info('Copying {0} to {1}...'.format(local_input_filepath, ip))
         sftp.put(local_input_filepath, input_filename)
         logger.info('Executing in {0}:'.format(ip))
         logger.info('{0}'.format(cmd))
@@ -146,38 +151,55 @@ if __name__ == "__main__":
 
     for i in range(num_examples):
         for d in [10, 20, 50, 100, 200, 500]:
-            for p in [2, 5, 10, 20]:  # p is number of data providers (not TI)
+            for p in [2, 3, 5]: #, 10, 20]:  # p is number of data providers (not TI)
                 if p > d:
                     continue
                 for n in [1000, 2000, 5000, 10000, 20000, 50000, 100000, 200000, 500000, 1000000]:
                     logger.info('Running instance n = {0}, d = {1}, p = {2}, run = {3}'.format(
                         n, d, p, i))
-                    filename_in = 'test_LR_{0}x{1}_{2}_{3}.in'.format(
-                        n, d, p, i)
-                    filepath_in = os.path.join(dest_folder, filename_in)
-                    (X, y, beta, e) = GT.generate_lin_regression(n, d, 0.1)
-                    GT.write_lr_instance(
-                        X, y, beta,
-                        filepath_in,
-                        p,
-                        private_endpoints)
-                    logger.info('Wrote instance in file {0}'.format(
-                        filepath_in))
+
                     party_exec_files = []
+                    party_out_files = []
                     for party in range(1, p + 3):
                         filename_exec = 'test_LR_{0}x{1}_{2}_{3}_p{4}.exec'\
                             .format(n, d, p, i, party)
                         filepath_exec = os.path.join(
                             dest_folder, filename_exec)
                         party_exec_files.append(filename_exec)
+                        party_out_files.append(os.path.splitext(
+                            filename_exec)[0] + '.out')
+
+                    party_out_filepaths = map(lambda x: os.path.join(dest_folder, x), party_out_files)
+                    if all([os.path.exists(party_out_filepaths[j - 1]) for j in range(1, p + 3)]) and\
+                       not RECOMPUTE:
+                        logger.info('Files {} exist. Skipping instance.'.format(', '.join(party_out_files)))
+                        continue
+
+                    filename_in = 'test_LR_{0}x{1}_{2}_{3}.in'.format(
+                        n, d, p, i)
+                    filepath_in = os.path.join(dest_folder, filename_in)
+                    if RECOMPUTE or not os.path.exists(filepath_in):
+                        (X, y, beta, e) = GT.generate_lin_regression(n, d, 0.1)
+                        GT.write_lr_instance(
+                            X, y, beta,
+                            filepath_in,
+                            p,
+                            private_endpoints)
+                        logger.info('Wrote instance in file {0}'.format(
+                            filepath_in))
+                    else:
+                        logger.info('Instance in file {0} exists. No need to generate it.'.format(
+                            filepath_in))
+
+                    for party in range(1, p + 3):
                         cmd = '{0} {1} {2} {3} > {4} 2>&1 {5}'.format(
                             exec_file,
                             filepath_in,
                             precision,
                             party,
-                            filepath_exec,
+                            os.path.join(dest_folder, party_exec_files[party - 1]),
                             '&' if party < p + 2 else '')
-
+                        
                         remote_working_dir = 'secure-distributed-linear-regression/'
 
                         run_remotely(remote_working_dir,
@@ -191,20 +213,27 @@ if __name__ == "__main__":
 
                     retrieve_out_files(party_exec_files,
                         dest_folder, dest_folder)
-                    data_parties_times = []
-                    for filename_exec in party_exec_files:
+                    for j, filename_exec in enumerate(party_exec_files):
+                        data_parties_byte_count = []
+                        data_parties_flush_count = []
                         filepath_exec = os.path.join(
                             dest_folder, filename_exec)
-                        filename_out = os.path.splitext(
-                            filename_exec)[0] + '.out'
 			filepath_out = os.path.join(
-			    dest_folder, filename_out)
+			    dest_folder, party_out_files[j])
                         f_exec = open(filepath_exec, 'r')
                         f_out = open(filepath_out, 'w')
                         logger.info('Writing .out file {0}'.format(filepath_out))
 			for line in f_exec.readlines():
                             if line.startswith('{'):
 				f_out.write(line)
+                            m = re.match('Total bytes sent: (\d+)', line)
+                            if m:
+				data_parties_byte_count.append(m.group(1))
+                            m = re.match('Total flush done: (\d+)', line)
+                            if m:
+				data_parties_flush_count.append(m.group(1))
+                        f_out.write('[{}]\n'.format(', '.join(data_parties_byte_count)))
+                        f_out.write('[{}]\n'.format(', '.join(data_parties_flush_count)))
                         f_exec.close()
                         f_out.close()
                     os.remove(filepath_in)
