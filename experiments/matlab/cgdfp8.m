e27368609243ec4f9c219967e568baa1d1955c51
agascon <adriagascon@gmail.com>
2016-09-28 11:16:39 +0100

    Clean up Borja's matlab code

--- experiments/matlab/cgdfp8.m
+++ experiments/matlab/cgdfp8.m
@@ -14,14 +14,10 @@ function [X,P,G] = cgdfp8(A,b,it,T) %#codegen
 	gammap = cast(zeros(d,1),'like',T);
 	g = cast(zeros(d,1),'like',T);
 	gscl = cast(zeros(d,1),'like',T);
-	%pscl = cast(zeros(d,1),'like',T);
 	pA = cast(zeros(1,d),'like',T);
-	%gdq = cast(zeros(d,1),'like',T);
 	etap = cast(zeros(d,1),'like',T);
 	etaAp = cast(zeros(d,1),'like',T);
-	%gdeta = cast(zeros(d,1),'like',T);
 	ng = cast(0,'like',T);
-	%ngnew = cast(0,'like',T);
 	gp = cast(0,'like',T);
 	gAp = cast(0,'like',T);
 	q = cast(0,'like',T);
@@ -38,35 +34,20 @@ function [X,P,G] = cgdfp8(A,b,it,T) %#codegen
 	p(:) = gscl;
 	for i = 1:it
 		display(sprintf('===== Iteration: %d =====',i));
-		%%%
 		pA(:) = p'*A;
 		q(:) = pA*p;    
-		%%% q = <p, Ap>
 		gp(:) = g'*p;
-		%eta(:) = gp/q;
 		eta(:) = mydivide(TT,gp,q);
-		%%% eta = <g,p> / <p,Ap>
 		etap(:) = eta*p;
 		x(:) = x - etap;
-		%%% x = x - eta*p
-		%Ax(:) = A*x;
-		%g(:) = Ax - b;
 		etaAp(:) = eta*(pA)';
 		g(:) = g - etaAp;
 		ng(:) = max(abs(g));
-		%ng(:) = 1.0;
-		%gscl(:) = g/ng;
 		gscl(:) = mydivide(TT,g,ng);
-		%gscl(:) = g;
-		%%%
 		gAp(:) = pA*gscl;
-		%gamma(:) = gAp/q;
 		gamma(:) = mydivide(TT,gAp,q);
-		%%% gamma = <g,Ap> / q
-		%%% (here eta = <g_old, g_old> / <p, Ap> and q = <p, Ap>)
 		gammap(:) = gamma*p;
 		p(:) = gscl - gammap;
-		%%% p = g + p * <g,g> / eta * q
 		display(sprintf('q=%.6e e=%.6e g=%.6e ng=%.6e gscl=%.6e np=%.6e ng=%.6e',double(q),double(eta),double(gamma),double(ng),norm(double(gscl))^2,norm(double(p))^2,norm(double(g))^2));
 		X(:,i) = x;
 		P(:,i) = p;
