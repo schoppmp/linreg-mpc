4096fa499e87de8661225cfb7cb4ae81dba2f6f4
Phillipp Schoppmann <schoppmann@informatik.hu-berlin.de>
2017-05-15 17:30:11 +0200

    Fix #8 by splitting off Obliv-C related parts of input.h

--- src/input.h
+++ src/input.h
@@ -12,9 +12,4 @@ void dcsClose(DualconS* con);
 
 DualconR* dcrConnect(node *self);
 void dcrRecvBitArray(DualconR* con,OblivBit* dest,size_t n,int party);
-#ifdef __oblivious_c
-static inline void
-dcrRecvIntArray(DualconR* con,obliv ufixed_t* input,size_t n,int party)
-  { dcrRecvBitArray(con,(OblivBit*)input,n*ocBitSize(*input),party); }
-#endif
 void dcrClose(DualconR* con);
